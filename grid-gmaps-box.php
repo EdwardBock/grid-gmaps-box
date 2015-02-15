b7d42f8afc35bc2a7a2894da4e898d8bf132f86a
Edward <edward.bock@palasthotel.de>
2015-02-15 10:20:34 +0100

    details

--- grid-gmaps-box.php
+++ grid-gmaps-box.php
@@ -1,11 +1,18 @@
 <?php
-/*
-Plugin Name: Grid GoogeMaps
-Plugin URI: http://edwardbock.de/
-Description: A box that renders GoogleMaps Api V3
-Version: 1.0
-Author: Edward Bock
-*/
+/**
+ * Plugin Name: Grid GoogleMaps Box
+ * Plugin URI: https://github.com/EdwardBock/grid-gmaps-box
+ * Description: Provides a GoogleMaps Box for Grid.
+ * Version: 1.0
+ * Author: Edward Bock <me@edwardbock.de>
+ * Author URI: http://www.edwardbock.de
+ * Requires at least: 4.0
+ * Tested up to: 4.1
+ * License: http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ * @copyright Copyright (c) 2015, Edward Bock
+ * @package Palasthotel\Grid-GoogleMaps-Box
+ */
+
 add_action("grid_load_classes", "grid_gmaps_box_load");
 
 function grid_gmaps_box_load(){
